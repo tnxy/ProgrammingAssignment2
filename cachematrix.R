4c6e68f8524419313bf58dea5c31cb2f88357bdd
tnxy <xiyu-020@163.com>
2016-03-01 23:35:38 +0800

    Assignment 2 for R programming
    Making two function to make a matrix inversed

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix funciton provides a list of functions including
+## intializing/getting matrix and setting/getting matrix inversion
+## by taking a matrix parameter.
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  
+  ##return a list object with four funcions
+  list(set = set, get = get,
+       setInverse = setInverse, 
+       getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve function can calculate matrix inversion with a 
+## makeCacheMatrix object. If matrix inversion has been calculated
+## an extra message will be prompted.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  ##if matrix inversion has been caculated, inversed matrix is returned directly.
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv
 }
